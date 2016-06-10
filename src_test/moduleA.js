85b93f86935b4d09e127cb70b77cd5aaf4b59bb2
rubencosta <rmc@nubolab.org>
2016-06-10 22:29:50 +0200

    Initial commit

---
+++ src_test/moduleA.js
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl'
+
+defineMessages({
+  a: {
+    id: 'a.a',
+    description: 'a message of the a module',
+    defaultMessage: 'the a message',
+  },
+  b: {
+    id: 'a.b',
+    description: 'b message of the a module',
+    defaultMessage: 'the b message',
+  },
+  c: {
+    id: 'a.c',
+    description: 'c message of the a module',
+    defaultMessage: 'the a message',
+  },
+})
---
+++ src_test/moduleB.js
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl'
+
+defineMessages({
+  a: {
+    id: 'b.a',
+    description: 'a message of the b module',
+    defaultMessage: 'the a message',
+  },
+  b: {
+    id: 'b.b',
+    description: 'b message of the b module',
+    defaultMessage: 'the b message',
+  },
+  c: {
+    id: 'b.c',
+    description: 'c message of the b module',
+    defaultMessage: 'the a message',
+  },
+})
