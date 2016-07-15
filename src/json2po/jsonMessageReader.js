1cca97d314bfb0619f01b1185b6f9df4a865d9b6
rubencosta <rmc@nubolab.org>
2016-07-15 12:08:30 +0200

    jsonReader: throw if duplicated ids are found

--- src/json2po/jsonMessageReader.js
+++ src/json2po/jsonMessageReader.js
@@ -5,20 +5,29 @@ import { sync as globSync } from 'glob'
 export default ({
   messagesPattern = '**/*.json',
   cwd = process.cwd(),
-}) => globSync(messagesPattern, { cwd })
-  .map(filename => {
-    const parsedMessages = JSON.parse(readFileSync(join(cwd, filename), 'utf-8'))
-    return ({
-      filename,
-      messages: Array.isArray(parsedMessages) ? parsedMessages : [],
+}) => {
+  const ids = new Set()
+  return globSync(messagesPattern, { cwd })
+    .map(filename => {
+      const parsedMessages = JSON.parse(readFileSync(join(cwd, filename), 'utf-8'))
+      return ({
+        filename,
+        messages: Array.isArray(parsedMessages) ? parsedMessages : [],
+      })
     })
-  })
-  .reduce((mappedMessages, { filename, messages }) => ([
-    ...mappedMessages,
-    ...messages.map(({ id, defaultMessage, description } = {}) => ({
-      reference: filename,
-      extracted: description,
-      msgstr: id,
-      msgid: defaultMessage,
-    })),
-  ]), [])
+    .reduce((mappedMessages, { filename, messages }) => ([
+      ...mappedMessages,
+      ...messages.map(({ id, defaultMessage, description } = {}) => {
+        if (ids.has(id)) {
+          throw new Error(`The id ${id} found in ${filename} was already defined`)
+        }
+        ids.add(id)
+        return ({
+          reference: filename,
+          extracted: description,
+          msgstr: id,
+          msgid: defaultMessage,
+        })
+      }),
+    ]), [])
+}
